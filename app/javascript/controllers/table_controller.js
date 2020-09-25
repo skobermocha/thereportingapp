821b86c5cbaa16f11a78f4b6c71634e70537f374
Jeremiah Ellis <skober@gmail.com>
2020-09-25 16:31:27 -0700

    Table Row link click to project_show

---
+++ app/javascript/controllers/table_controller.js
@@ -0,0 +1,14 @@
+// table_controller.js
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  
+  click(e){
+    Turbolinks.visit(this.url)
+  }
+
+  get url() {
+    return this.targets.find('url').href
+  }
+
+}
