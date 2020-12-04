ca37ad3c4de2b93ba0f073dbd756037a15d131b5
Jeremiah Ellis <skober@gmail.com>
2020-12-03 17:10:17 -0800

    CZ look up seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 # Uncomment the following to create an Admin user for Production in Jumpstart Pro
 # User.create name: "name", email: "email", password: "password", password_confirmation: "password", admin: true, terms_of_service: true
+
+
+### Zipcode for CZ lookup
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'CZByZIPCode.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Czlookup.new
+  t.zipcode = row['zipcode']
+  t.climatezone = row['climatezone']
+  t.save
+  #puts "#{t.zipcode}, #{t.climatezone} saved"
+end
+
+puts "There are now #{Czlookup.count} rows in the ClimateZone table"
